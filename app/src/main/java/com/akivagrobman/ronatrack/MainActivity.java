d9d52c70a7129283c36db73dbe6b6925e21c9da1
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2020-12-05 21:47:03 +0200

    checking first time login update

--- app/src/main/java/com/akivagrobman/ronatrack/MainActivity.java
+++ app/src/main/java/com/akivagrobman/ronatrack/MainActivity.java
@@ -2,13 +2,38 @@ package com.akivagrobman.ronatrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static SharedPreferences sharedPreferences;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    public static final String IS_FIRST_TIME = "is_first_time";
+    public static boolean isFirstTime = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences(TAG, MODE_PRIVATE);
+        isFirstTime = sharedPreferences.getBoolean(IS_FIRST_TIME, true);
+
+        if(isFirstTime){
+          //open the first time login todo : create the first time login
+            //...
+            //...
+        }
+
     }
+
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(IS_FIRST_TIME, isFirstTime);
+        editor.apply();
+    }
+
 }
